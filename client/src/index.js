894c65595c91b4fb91bec62ccd2e7d53181b5f0f
Chris Gambrell <christopher@gambrell.info>
2022-03-18 17:32:53 -0500

    Configure react-query

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import App from './App'
+import './index.css'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: { onError: (error) => window.alert(error) },
+		mutations: { onError: (error) => window.alert(error) },
+	},
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<App />
+			<ReactQueryDevtools />
+		</QueryClientProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
+)
