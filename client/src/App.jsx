38ed9cb1527eeefb076ba0359a7002b2e306b600
Chris Gambrell <christopher@gambrell.info>
2022-03-18 22:29:09 -0500

    Animation for task select

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useCreateTask, useDeleteTask, useEditTask, useTasks } from './hooks'
-import { Col, Dropdown, Form, Input, Menu, Row } from 'antd'
+import { Card, Col, Dropdown, Form, Input, Menu, Row } from 'antd'
 import './App.min.css'
 
 const App = () => {
@@ -9,6 +9,7 @@ const App = () => {
 	const deleteTask = useDeleteTask().mutate
 	const editTask = useEditTask().mutate
 
+	const [selectedTask, setSelectedTask] = useState(-1)
 	const [title, setTitle] = useState('')
 
 	const handleCreateTask = async (e) => {
@@ -33,9 +34,21 @@ const App = () => {
 							<li
 								className='task-item'
 								key={task.id}
+								onDoubleClick={() => setSelectedTask(task.id)}
 								onContextMenu={(e) => e.preventDefault()}
 								style={{
-									padding: '5px 10px',
+									margin:
+										selectedTask === task.id ? '25px 0' : 0,
+									padding:
+										selectedTask === task.id
+											? 15
+											: '5px 10px',
+									borderRadius: 5,
+									boxShadow:
+										selectedTask === task.id
+											? '0 1px 2px -2px rgba(0,0,0,.16),0 3px 6px 0 rgba(0,0,0,.12),0 5px 12px 4px rgba(0,0,0,.09)'
+											: 'none',
+									transition: 'all 0.2s',
 								}}>
 								<Dropdown
 									overlay={
