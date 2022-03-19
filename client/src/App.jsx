64e233b6e58bc1b0af13c36c216f7a6dad40e585
Chris Gambrell <christopher@gambrell.info>
2022-03-18 22:07:34 -0500

    Add menu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useCreateTask, useDeleteTask, useEditTask, useTasks } from './hooks'
-import { Dropdown, Form, Input, Menu } from 'antd'
+import { Col, Dropdown, Form, Input, Menu, Row } from 'antd'
 import './App.min.css'
 
 const App = () => {
@@ -19,60 +19,69 @@ const App = () => {
 	}
 
 	return (
-		<div
-			className='App'
-			style={{ maxWidth: 600, margin: '25px auto 0 auto' }}>
-			<h1>Tasks &infin;</h1>
-			<ul style={{ listStyle: 'none' }}>
-				{tasks?.map((task) => (
-					<li
-						className='task-item'
-						key={task.id}
-						onDoubleClick={() => deleteTask(task.id)}
-						onContextMenu={(e) => e.preventDefault()}
-						style={{
-							padding: '5px 10px',
-						}}>
-						<Dropdown
-							overlay={
-								<Menu>
-									<Menu.Item
-										key='delete'
-										danger
-										onClick={() => deleteTask(task.id)}>
-										Delete task
-									</Menu.Item>
-								</Menu>
-							}
-							trigger={['contextMenu']}>
-							<div>
-								<input
-									type='checkbox'
-									checked={task.completed}
-									onChange={() =>
-										editTask({
-											taskId: task.id,
-											data: {
-												completed: !task.completed,
-											},
-										})
+		<div className='App'>
+			<Row style={{ margin: '25px 0' }}>
+				<Col sm={5}>
+					<h1 style={{ marginLeft: 25 }}>Tasks &infin;</h1>
+					<Menu defaultSelectedKeys={['1']} mode='inline'>
+						<Menu.Item key='1'>Tasks</Menu.Item>
+					</Menu>
+				</Col>
+				<Col sm={19} style={{ maxWidth: 600, margin: '0 auto' }}>
+					<ul style={{ listStyle: 'none' }}>
+						{tasks?.map((task) => (
+							<li
+								className='task-item'
+								key={task.id}
+								onContextMenu={(e) => e.preventDefault()}
+								style={{
+									padding: '5px 10px',
+								}}>
+								<Dropdown
+									overlay={
+										<Menu>
+											<Menu.Item
+												key='delete'
+												danger
+												onClick={() =>
+													deleteTask(task.id)
+												}>
+												Delete task
+											</Menu.Item>
+										</Menu>
 									}
-									style={{ marginRight: 10 }}
-								/>
-								{task.title}
-							</div>
-						</Dropdown>
-					</li>
-				))}
-			</ul>
-			<Form onSubmitCapture={handleCreateTask}>
-				<Input
-					type='text'
-					placeholder='New task'
-					value={title}
-					onChange={(e) => setTitle(e.target.value)}
-				/>
-			</Form>
+									trigger={['contextMenu']}>
+									<div>
+										<input
+											type='checkbox'
+											checked={task.completed}
+											onChange={() =>
+												editTask({
+													taskId: task.id,
+													data: {
+														completed:
+															!task.completed,
+													},
+												})
+											}
+											style={{ marginRight: 10 }}
+										/>
+										{task.title}
+									</div>
+								</Dropdown>
+							</li>
+						))}
+					</ul>
+					<Form onSubmitCapture={handleCreateTask}>
+						<Input
+							type='text'
+							placeholder='New task'
+							value={title}
+							onChange={(e) => setTitle(e.target.value)}
+						/>
+					</Form>
+				</Col>
+			</Row>
 		</div>
 	)
 }
