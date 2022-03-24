3a391f8a235780a8970f94baf1df2b2a2fbb3d46
Chris Gambrell <christopher@gambrell.info>
2022-03-24 19:00:00 -0400

    Seed for new when property

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -8,3 +8,5 @@
 
 Task.create(title: 'Double-click this to-do', notes: 'You\'re looking at a to-do! Complete it by clicking the checkbox on the left. Completed to-dos are collected at the bottom of your project.')
 Task.create(title: 'Create a new to-do', notes: 'Your turn - just hit the plus button on the toolbar at the bottom.')
+Task.create(title: 'Add this to-do to your Today list', notes: 'Click the calendar button below to plan when you\'ll do this to-do. Choose Today.')
+Task.create(title: 'Plan this to-do for later', notes: 'Click the calendar button again, but this time, choose a date in the future. It will automatically appear in your Today list when the date comes.\n\nUpcoming to-dos are grouped together at the bottom of each section.')
