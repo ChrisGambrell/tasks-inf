dc29409ac72a24c0c8a094953a9677efe9283d1f
Chris Gambrell <christopher@gambrell.info>
2022-03-28 22:11:16 -0400

    Separate context menu from TaskList

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -1,14 +1,13 @@
 import { useContext } from 'react'
-import { Col, Checkbox, Dropdown, Menu, Row, Typography } from 'antd'
+import { Col, Checkbox, Dropdown, Row, Typography } from 'antd'
 import { blue } from '@ant-design/colors'
 import { File, Star } from 'tabler-icons-react'
-import { useDeleteTask, useEditTask, useTasks } from '../hooks'
+import { useEditTask, useTasks } from '../hooks'
 import { TasksContext } from '../App'
-import { TaskForm } from '.'
+import { TaskContextMenu, TaskForm } from '.'
 
 const TaskList = ({ query: listQuery = {} }) => {
 	const { data: tasks } = useTasks()
-	const deleteTask = useDeleteTask().mutate
 	const editTask = useEditTask().mutate
 
 	const [state, dispatch] = useContext(TasksContext)
@@ -41,12 +40,6 @@ const TaskList = ({ query: listQuery = {} }) => {
 			dispatch({ type: 'set', payload: { selected: [taskId], open: -1, showAddingTask: false } })
 	}
 
-	const handleDeleteTask = async (taskId = state.open) => {
-		if (state.selected.length > 1) await state.selected.map((tId) => deleteTask(tId))
-		else await deleteTask(taskId)
-		dispatch({ type: 'set', payload: { open: -1 } })
-	}
-
 	return (
 		<ul style={{ listStyle: 'none' }}>
 			{state.showAddingTask && <TaskForm className='task-item active' type='create' as='li' />}
@@ -75,68 +68,7 @@ const TaskList = ({ query: listQuery = {} }) => {
 						onDoubleClick={() => dispatch({ type: 'set', payload: { open: task.id, selected: [] } })}
 						onContextMenu={(e) => e.preventDefault()}
 						style={{ backgroundColor: state.selected.includes(task.id) ? blue[1] : 'white' }}>
-						<Dropdown
-							overlay={
-								<Menu id='contextmenu'>
-									<Menu.Item key='when' onClick={() => console.log('TODO')}>
-										When...
-									</Menu.Item>
-									<Menu.Item key='move' onClick={() => console.log('TODO')}>
-										Move...
-									</Menu.Item>
-									<Menu.Item key='tags' onClick={() => console.log('TODO')}>
-										Tags...
-									</Menu.Item>
-									<Menu.Item key='deadline' onClick={() => console.log('TODO')}>
-										Deadline...
-									</Menu.Item>
-									<Menu.SubMenu key='complete' title='Complete'>
-										<Menu.Item key='mark-as-completed' onClick={() => console.log('TODO')}>
-											Mark as Completed
-										</Menu.Item>
-										<Menu.Item key='mark-as-cancelled' onClick={() => console.log('TODO')}>
-											Mark as Cancelled
-										</Menu.Item>
-									</Menu.SubMenu>
-									<Menu.SubMenu key='shortcuts' title='Shortcuts'>
-										<Menu.ItemGroup title='When'>
-											<Menu.Item key='today' onClick={() => console.log('TODO')}>
-												Today
-											</Menu.Item>
-											<Menu.Item key='this-evening' onClick={() => console.log('TODO')}>
-												This Evening
-											</Menu.Item>
-											<Menu.Item key='someday' onClick={() => console.log('TODO')}>
-												Someday
-											</Menu.Item>
-											<Menu.Item key='clear' onClick={() => console.log('TODO')}>
-												Clear
-											</Menu.Item>
-										</Menu.ItemGroup>
-									</Menu.SubMenu>
-									<Menu.Divider />
-									<Menu.Item key='repeat' onClick={() => console.log('TODO')}>
-										Repeat...
-									</Menu.Item>
-									<Menu.Item key='get-info' onClick={() => console.log('TODO')}>
-										Get Info...
-									</Menu.Item>
-									<Menu.Item key='duplicate-task' onClick={() => console.log('TODO')}>
-										Duplicate Task
-									</Menu.Item>
-									<Menu.Item key='convert-to-project' onClick={() => console.log('TODO')}>
-										Convert to Project...
-									</Menu.Item>
-									<Menu.Item key='delete' danger onClick={() => handleDeleteTask(task.id)}>
-										Delete Task
-									</Menu.Item>
-									<Menu.Divider />
-									<Menu.Item key='remove-from-project' onClick={() => console.log('TODO')}>
-										Remove From Project
-									</Menu.Item>
-								</Menu>
-							}
-							trigger={['contextMenu']}>
+						<Dropdown overlay={<TaskContextMenu taskId={task.id} />} trigger={['contextMenu']}>
 							<div>
 								{state.open === task.id ? (
 									<TaskForm type='edit' task={task} />
