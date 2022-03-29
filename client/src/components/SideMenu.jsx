90653f4da87ccc3be7622685fda4b35c2f82b784
Chris Gambrell <christopher@gambrell.info>
2022-03-29 11:31:46 -0400

    Update SideMenu

--- client/src/components/SideMenu.jsx
+++ client/src/components/SideMenu.jsx
@@ -1,51 +1,107 @@
 import { useNavigate } from 'react-router-dom'
-import { Group, Navbar, Text, ThemeIcon, Title, UnstyledButton } from '@mantine/core'
+import { Navbar, Title, createStyles } from '@mantine/core'
 import { Archive, Calendar, Inbox, Infinity, Notebook, Stack2, Star, Trash } from 'tabler-icons-react'
 
+const useStyles = createStyles((theme, _params, getRef) => {
+	const icon = getRef('icon')
+
+	return {
+		navbar: {
+			backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.white,
+		},
+
+		title: {
+			textTransform: 'uppercase',
+			letterSpacing: -0.25,
+		},
+
+		link: {
+			...theme.fn.focusStyles(),
+			display: 'flex',
+			alignItems: 'center',
+			textDecoration: 'none',
+			fontSize: theme.fontSizes.sm,
+			color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[7],
+			padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
+			borderRadius: theme.radius.sm,
+			fontWeight: 500,
+
+			'&:hover': {
+				backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[0],
+				color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+
+				[`& .${icon}`]: {
+					color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+				},
+			},
+		},
+
+		linkIcon: {
+			ref: icon,
+			color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6],
+			marginRight: theme.spacing.sm,
+		},
+
+		linkActive: {
+			'&, &:hover': {
+				backgroundColor:
+					theme.colorScheme === 'dark'
+						? theme.fn.rgba(theme.colors[theme.primaryColor][9], 0.25)
+						: theme.colors[theme.primaryColor][0],
+				color: theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 4 : 7],
+				[`& .${icon}`]: {
+					color: theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 4 : 7],
+				},
+			},
+		},
+
+		footer: {
+			borderTop: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]}`,
+			paddingTop: theme.spacing.md,
+		},
+	}
+})
+
 const SideMenu = () => {
+	const { classes } = useStyles()
+
 	return (
-		<Navbar p='xs' width={{ base: 200 }}>
+		<Navbar width={{ sm: 300 }} p='md' className={classes.navbar}>
 			<Navbar.Section>
 				<Title order={1}>
 					Tasks <Infinity />
 				</Title>
 			</Navbar.Section>
-			<Navbar.Section grow mt='md'>
-				<SideMenuItem icon={<Inbox />} color='blue' label='Inbox' to='/' />
-				<SideMenuItem icon={<Star />} color='yellow' label='Today' to='/today' />
-				<SideMenuItem icon={<Calendar />} color='red' label='Upcoming' to='TODO' />
-				<SideMenuItem icon={<Stack2 />} color='teal' label='Anytime' to='TODO' />
-				<SideMenuItem icon={<Archive />} color='gray' label='Someday' to='TODO' />
-				<SideMenuItem icon={<Notebook />} color='green' label='Logbook' to='TODO' />
-				<SideMenuItem icon={<Trash />} color='gray' label='Trash' to='TODO' />
-				<SideMenuItem icon={<Infinity />} color='white' label='Meet Tasks' to='/' />
+
+			<Navbar.Section grow mt='xl'>
+				<SideMenuItem icon={Inbox} label='Inbox' to='/' />
+				<SideMenuItem icon={Star} label='Today' to='/today' />
+				<SideMenuItem icon={Calendar} label='Upcoming' to='TODO' />
+				<SideMenuItem icon={Stack2} label='Anytime' to='TODO' />
+				<SideMenuItem icon={Archive} label='Someday' to='TODO' />
+				<SideMenuItem icon={Notebook} label='Logbook' to='TODO' />
+				<SideMenuItem icon={Trash} label='Trash' to='TODO' />
+				<SideMenuItem icon={Infinity} label='Meet Tasks' to='/' />
 			</Navbar.Section>
 		</Navbar>
 	)
 }
 
-const SideMenuItem = ({ icon, color, label, to }) => {
+const SideMenuItem = ({ label, icon: Icon, to }) => {
 	const navigate = useNavigate()
+	const { classes, cx } = useStyles()
 
 	return (
-		<UnstyledButton
-			sx={(theme) => ({
-				display: 'block',
-				width: '100%',
-				padding: 5,
-				borderRadius: theme.radius.md,
-				color: theme.black,
-				...(window.location.pathname === to && { backgroundColor: theme.colors.gray[0] }),
-				'&:hover': { backgroundColor: theme.colors.gray[0] },
-			})}
-			onClick={() => navigate(to)}>
-			<Group>
-				<ThemeIcon color={color} variant='light'>
-					{icon}
-				</ThemeIcon>
-				<Text>{label}</Text>
-			</Group>
-		</UnstyledButton>
+		<a
+			className={cx(classes.link, { [classes.linkActive]: to === window.location.pathname })}
+			key={label}
+			onClick={(e) => {
+				e.preventDefault()
+				navigate(to)
+			}}>
+			<Icon className={classes.linkIcon} />
+			<span>{label}</span>
+		</a>
 	)
 }
 
